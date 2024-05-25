c8c5ef8d66552d45a0fc736c210cfc9f7f08065b
Subathra Manoharan <smanoharan@infoane.com>
2024-05-25 16:09:01 +0530

    Navbar Modifies

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,18 +14,15 @@ const Navbar = () => {
         <ul className='Navbar-Menu'>
             <li onClick={() => setMenu("Home")} className={Menu === "Home" ? "hover" : "" }>Home</li>
             <li onClick={() => setMenu("Menu")} className={Menu === "Menu" ? "active" : "" }>Menu</li>
-            <li onClick={() => setMenu("Order Online")} className={Menu === "Order Online" ? "active" : "" }>Order Online</li>
             <li onClick={() => setMenu("About Us")} className={Menu === "About Us" ? "active" : "" }>About Us</li>
-            <li onClick={() => setMenu("Specials")} className={Menu === "Specials" ? "active" : "" } >Specials</li>
-            <li onClick={() => setMenu("Locations")} className={Menu === "Locations" ? "active" : "" }>Locations</li>
+            <li onClick={() => setMenu("Today Special's")} className={Menu === "Specials" ? "active" : "" } >Today Special's</li>
             <li onClick={() => setMenu("Account")} className={Menu === "Account" ? "active" : "" }>Account</li>
-            <li onClick={() => setMenu("Cart")} className={Menu === "Cart" ? "active" : "" }>Cart</li>
             <li onClick={() => setMenu("Contct Us")} className={Menu === "Contact Us" ? "active" : "" }>Contact Us</li>
         </ul>
         <div className="Navbar-right">
             <IoSearch />
             <div className="Navbar-cart">
-                <IoCart />
+                <IoCart/> 
             </div>
             <div className="Navbar-signin">
                 <button>Sign up</button>
