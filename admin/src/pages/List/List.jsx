a8d959f662bbf6af07ab8d6b86d6a753b617d5fa
Vasanth S <91889989+VasanthSM@users.noreply.github.com>
2024-06-21 13:01:29 +0530

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 
 const List = () => {
   const [list, setList] = useState([]);
-  const url = 'https://pizzaman-backend.onrender.com'
+  const url = "https://pizzaman-backend.onrender.com"
 
   const fetchList = async () => {
     try {
